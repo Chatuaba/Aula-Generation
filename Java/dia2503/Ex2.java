c55e1d2fa0f919ac7fa42ef9016c53dabff823b3
Eduardo <eduardoclacalle@gmail.com>
2021-03-25 15:24:18 -0300

    correção

--- Java/dia2503/Ex2.java
+++ Java/dia2503/Ex2.java
@@ -12,23 +12,23 @@ public class Ex2 {
 		a=l.nextInt();
 		b=l.nextInt();
 		c=l.nextInt();
-		if(a>b&& a>c) {
-			if(b>c) {
-				System.out.printf("%d > %d > %d",a,b,c);
+		if(a<b&& a<c) {
+			if(b<c) {
+				System.out.printf("%d < %d < %d",a,b,c);
 			}else {
-				System.out.printf("%d > %d > %d",a,c,b);
+				System.out.printf("%d < %d < %d",a,c,b);
 			}
-		}else if(b>a && b>c) {
-			if(a>c) {
-				System.out.printf("%d > %d > %d",b,a,c);
+		}else if(b<a && b<c) {
+			if(a<c) {
+				System.out.printf("%d < %d < %d",b,a,c);
 			}else {
-				System.out.printf("%d > %d > %d",b,c,a);
+				System.out.printf("%d < %d < %d",b,c,a);
 			}
 		}else {
-			if(a>b) {
-				System.out.printf("%d > %d > %d",c,a,b);
+			if(a<b) {
+				System.out.printf("%d < %d < %d",c,a,b);
 			}else {
-				System.out.printf("%d > %d > %d",c,b,a);
+				System.out.printf("%d < %d < %d",c,b,a);
 			}
 		}
 		
